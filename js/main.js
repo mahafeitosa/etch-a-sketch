88b83bea7e6a6fd176e9ae811db4664b0b483012
mahafeitosa <mahafeitosa@icloud.com>
2022-12-11 16:31:15 -0300

    Work with buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
+let color = 'black';
+
 document.addEventListener('DOMContentLoaded', function () {
   createBoard(16); // Change number to set board size
-  console.log('Hello there!');
+  let btnPopup = document.querySelector('#popup');
+  btnPopup.addEventListener('click', () => {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 // Function to set size of board
@@ -14,6 +20,39 @@ function createBoard(size) {
   let numDivs = size * size;
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement('div');
+    div.addEventListener('mouseover', colorDiv);
     board.insertAdjacentElement('beforeend', div);
   }
+}
+
+// Get user's input to choose board size
+function getSize() {
+  let userInput = prompt('Choose size of the board');
+  let message = document.querySelector('#message');
+  if (userInput == '') {
+    message.innerHTML = 'Please choose a number';
+  } else if (userInput < 0 || userInput > 100) {
+    message.innerHTML = 'Please choose a number above 0 and below 100';
+  } else {
+    message.innerHTML = 'Have fun sketching!';
+    return userInput;
+  }
+}
+
+function colorDiv() {
+  if (color == 'random') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+// Reset board to white
+function resetBoard() {
+  let divs = document.querySelectorAll('div');
+  divs.forEach((div) => (div.style.backgroundColor = 'White'));
 }
